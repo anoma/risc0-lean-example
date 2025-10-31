f94d696cafcb06917e7abb273deca5989e226cbb
Lukasz Czajka <lukasz@heliax.dev>
2025-10-31 17:47:44 +0100

    remove initialization code

--- methods/guest/risc0_lean.c
+++ methods/guest/risc0_lean.c
@@ -6,12 +6,12 @@ extern lean_object* initialize_Guest(uint32_t builtin, lean_object* world);
 extern void lean_initialize_runtime_module();
 
 uint32_t lean_simple_risc0_main(uint32_t n) {
-    lean_initialize_runtime_module();
-    lean_object* res = initialize_Guest(1, lean_io_mk_world());
-    if (!lean_io_result_is_ok(res)) {
-        lean_dec_ref(res);
-        return 0;
-    }
-    lean_dec_ref(res);
+    // lean_initialize_runtime_module();
+    // lean_object* res = initialize_Guest(1, lean_io_mk_world());
+    // if (!lean_io_result_is_ok(res)) {
+    //     lean_dec_ref(res);
+    //     return 0;
+    // }
+    // lean_dec_ref(res);
     return risc0_main(n);
 }
